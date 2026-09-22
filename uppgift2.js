952cbe4b76f8da270554f80e10dc6e3cbe231508
Linus Falk <linuspop77@gmail.com>
2026-09-22 19:32:44 +0200

    Skrev till de sista terminal raderna och gjorde den sista ekvationen.

--- uppgift2.js
+++ uppgift2.js
@@ -9,8 +9,12 @@ Författare: Linus Falk.
 let milk = 16;
 const amountMilk = 10;
 
-// Ekvation
+// Ekvationer
 const totalAmountPrice = milk * amountMilk;
+const totalAmountPriceVat = totalAmountPrice * 1.25;
 
 // Kod för att skriva ut i terminalen
-console.log(totalAmountPrice);
+console.log("Pris: " + milk);
+console.log("Antal: " + amountMilk);
+console.log("Totalt: " + totalAmountPrice);
+console.log("Totalt inkl moms: " + totalAmountPriceVat);
