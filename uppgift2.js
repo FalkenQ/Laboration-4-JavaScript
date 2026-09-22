15fccbcb5d7a5b122fdc02f8a220388975c01ddd
Linus Falk <linuspop77@gmail.com>
2026-09-22 19:40:51 +0200

    Ändrade milk från let till const. satte dit let innan med tanken att priset varierar, men insåg att den inte gör detta i uppgiften såklart. Gjorde även console koden snyggare.

--- uppgift2.js
+++ uppgift2.js
@@ -1,20 +1,20 @@
 /*
-
+Detta är ett program som räknar ut priset med moms och utan moms på antalet mjölk kartonger.
 Författare: Linus Falk.
 */
 
 "use strict";
 
 // Variabler och konstanter
-let milk = 16;
+const milk = 16;
 const amountMilk = 10;
 
-// Ekvationer
+// Ekvationer för att räkna ut pris på antelt mjölk och mjölk + moms
 const totalAmountPrice = milk * amountMilk;
 const totalAmountPriceVat = totalAmountPrice * 1.25;
 
 // Kod för att skriva ut i terminalen
-console.log("Pris: " + milk);
-console.log("Antal: " + amountMilk);
-console.log("Totalt: " + totalAmountPrice);
-console.log("Totalt inkl moms: " + totalAmountPriceVat);
+console.log("Pris: " + milk + " kr/st");
+console.log("Antal: " + amountMilk + " st");
+console.log("Totalt: " + totalAmountPrice + " kr");
+console.log("Totalt inkl moms: " + totalAmountPriceVat + " kr");
