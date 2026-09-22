da0f6d53d855646dddde946fc571a42a69250be4
Linus Falk <linuspop77@gmail.com>
2026-09-22 19:29:36 +0200

    Skrivit första ekvationen för totala priset för antalet paket mjölk.

---
+++ uppgift2.js
@@ -0,0 +1,16 @@
+/*
+
+Författare: Linus Falk.
+*/
+
+"use strict";
+
+// Variabler och konstanter
+let milk = 16;
+const amountMilk = 10;
+
+// Ekvation
+const totalAmountPrice = milk * amountMilk;
+
+// Kod för att skriva ut i terminalen
+console.log(totalAmountPrice);
