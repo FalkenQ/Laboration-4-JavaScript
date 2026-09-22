58aa398c70fde52fd40218b8b81e7bf3cf810b5e
Linus Falk <linuspop77@gmail.com>
2026-09-22 19:12:54 +0200

    Skrev alla variabler och konstanter. Därefter skrev jag koden för att programmet skulle kunna skriva ut det. avslutade med att kommentera.

---
+++ uppgift1.js
@@ -0,0 +1,17 @@
+/*
+Detta är ett javascript som lagrar namn, ålder och ifall personen är en student.
+För att kunna skriva ut det i terminalen.
+Linus Falk.
+*/
+
+"use strict";
+// Variabler och konstanter
+let firstName = "Michelle";
+let lastName = "Thorell";
+const age = 25;
+let isStudent = true;
+
+// Kod som skriver ut namn, ålder och om personen är en student
+console.log(firstName + " " + lastName);
+console.log("Ålder: " + age);
+console.log("Student: " + isStudent);
