a37eb08f3d34168aba88f0394dd4bd1afe0c3609
Linus Falk <linuspop77@gmail.com>
2026-09-22 19:20:57 +0200

    Blev strul så min tidigare commit, och kom inte med. Skrev till variabler och konstanter. Efter skrev jag koden för att skriva ut efterfrågad utfall i terminalen. Avslutade med att skriva kommentarer.

--- uppgift1.js
+++ uppgift1.js
@@ -5,13 +5,13 @@ Författare: Linus Falk.
 */
 
 "use strict";
-// Variabler och konstanter
+// Variabler och konstanter.
 let firstName = "Michelle";
 let lastName = "Thorell";
 const age = 25;
 let isStudent = true;
 
-// Kod som skriver ut namn, ålder och om personen är en student
+// Kod som skriver ut namn, ålder och om personen är en student.
 console.log(firstName + " " + lastName);
 console.log("Ålder: " + age);
 console.log("Student: " + isStudent);
