ffeec805863da88b13450ce4a21df39faab7da78
Linus Falk <linuspop77@gmail.com>
2026-09-23 09:16:49 +0200

    Skapade en konstant för ålder och kodade den första if satsen för att checka om personen är vuxen.

---
+++ uppgift3.js
@@ -0,0 +1,12 @@
+/*
+Detta är ett program som räknar ut priset med moms och utan moms på antalet mjölk kartonger.
+Författare: Linus Falk.
+*/
+
+"use strict";
+
+const age = 25;
+
+if (age > 17) {
+    console.log("Du är vuxen.");
+}
