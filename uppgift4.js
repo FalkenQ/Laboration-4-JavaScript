655c11fdb3154aa26109b55c1ee0cf6fa158cabe
Linus Falk <linuspop77@gmail.com>
2026-09-23 09:50:43 +0200

    Skapade en for loop som skriver ut talen mellan 1-20.

---
+++ uppgift4.js
@@ -0,0 +1,13 @@
+/*
+Detta är ett program som kollar personens ålder och ser ifall personen antigen räknas som ett barn,
+vuxen/myndig eller en pensionär och även ifall personen har gjort en korrekt inmatning med sin ålder.
+koden innehåller if, else if, och else satser.
+Författare: Linus Falk.
+*/
+
+"use strict";
+
+
+for (let i = 1; i <= 20; i++ ) {
+    console.log("Numret är " + i);
+}
