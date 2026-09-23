c99e3c6c947618f49f3279ba6f1ae1d15aaec94f
Linus Falk <linuspop77@gmail.com>
2026-09-23 10:02:38 +0200

    Kommentarer.

--- uppgift4.js
+++ uppgift4.js
@@ -1,17 +1,18 @@
 /*
-Detta är ett program som kollar personens ålder och ser ifall personen antigen räknas som ett barn,
-vuxen/myndig eller en pensionär och även ifall personen har gjort en korrekt inmatning med sin ålder.
-koden innehåller if, else if, och else satser.
+Detta är ett program som skriver ut alla tal mellan 1-20 och även alla jämna tal mellan 1-20.
+Koden består av två for loopar. Den första skriver ut alla tal 1-20.
+for loop två skriver bara ut de jämna talen i talföljden 1-20.
 Författare: Linus Falk.
 */
 
 "use strict";
 
-
+// for loop för talföljden 1-20
 for (let i = 1; i <= 20; i++ ) {
     console.log("Numret är " + i);
 }
 
+// for loop för talföljden 1-20 men bara jämna tal
 for (let i = 1; i <= 20; i++) {
     if (i % 2 == 0) {
         console.log("Det jämna talet är " + i);
