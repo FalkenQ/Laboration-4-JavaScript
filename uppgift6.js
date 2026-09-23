600108ed091e7f499938935f3936e3dd67576b6b
Linus Falk <linuspop77@gmail.com>
2026-09-23 11:38:45 +0200

    Skapat funktionen för att beräkna arean av en rektangel.

---
+++ uppgift6.js
@@ -0,0 +1,19 @@
+/*
+Detta är ett program för att skapa en "food" array med 5 måltider.
+Programmet skriver ut hela arrayen, första elementet, sista elementet.
+Programmet lägger till en måltid/element sist i arrayen.
+Programmet tar bort första måltiden/elementet och flyttar ner alla element ett steg.
+Sist skriver programmet ut de nuvarande elementen/måltider som finns i food arrayen.
+
+Programmet innehåller en array som sen använder sig av push och shift för att utföra sin uppgift.
+
+Författare: Linus Falk.
+*/
+
+"use strict";
+
+// Funktionen för att beräkna arean på en rektangel
+function calculateArea(a, b) {
+   const area = a * b;
+   return area;
+}
