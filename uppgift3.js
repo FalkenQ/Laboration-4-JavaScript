d251e99c894a1ffceaf108de85ba5bdcceda8fca
Linus Falk <linuspop77@gmail.com>
2026-09-23 09:37:19 +0200

    Skapade de resterande if satserna för att kolla om personen är vuxen, pensionär eller om åldern är korrekt. Har lagt till kommentarer.

--- uppgift3.js
+++ uppgift3.js
@@ -1,14 +1,22 @@
 /*
-Detta är ett program som räknar ut priset med moms och utan moms på antalet mjölk kartonger.
+Detta är ett program som kollar personens ålder och ser ifall personen antigen räknas som ett barn,
+vuxen/myndig eller en pensionär och även ifall personen har gjort en korrekt inmatning med sin ålder.
+koden innehåller if, else if, och else satser.
 Författare: Linus Falk.
 */
 
 "use strict";
 
-const age = 16;
+// Konstant för ålder
+const age = -7;
 
-if (age > 17) {
-    console.log("Du är myndig.");
+// if satser för att checka om man är under 18, mellan 18-65, 65 eller över samt om personen har gjort en korrekt inmatning av ålder
+if (age > 17 && age < 65 && age >= 0) {
+    console.log("Du är " + age + " år. Så du är myndig.");
+} else if (age < 0) {
+    console.log("Felaktig inmatning.");
 } else if (age < 18) {
-    console.log("Du är fortfarande inte myndig.")
+    console.log("Du är fortfarande inte myndig. Du är " + age + " år");
+} else {
+    console.log("Grattis, du får pensionär rabatt. Du har snurrat runt jorden " + age + " gånger.");
 }
